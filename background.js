0fbe7457c2ca878b397b5ae18ccbba41b08b176f
Beeven Yip <beeven@hotmail.com>
2021-08-14 17:29:01 +0800

    init commit

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(data => {
+    if (data.type === 'notification') {
+      chrome.notifications.create('', data.options);
+    }
+  });
+
+chrome.action.onClicked.addListener(async (tab) => {
+  let queryOptions = {url:"*://mail.hg.cn/*"};
+  let [t] = await chrome.tabs.query(queryOptions);
+  if(t) {
+    console.log(t);
+    chrome.tabs.highlight({tabs: t.index, windowId: t.windowId});
+  } else {
+    chrome.tabs.create({active: true, url: "http://mail.hg.cn/webmailgo.php"});
+  }
+  
+});
